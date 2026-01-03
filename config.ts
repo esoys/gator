71e89c2610fcbe4c2b088385185b99da3f64afdd
esoys <erik@soysal.de>
2026-01-03 10:08:29 +0100

    config

---
+++ config.ts
@@ -0,0 +1,11 @@
+import fs from "fs";
+import os from "os";
+import path from "path";
+
+export type Config = {
+    dbUrl: string;
+    currentUserName: string;
+};
+
+export function setUser(): void {
+};
